3f29a04daade15ae3ced2449d451b772964d19a5
Imtiny <tzhang20200709@gmail.com>
2020-05-12 23:27:01 +0800

    add simple slogan component

--- web/src/pages/GFashion/index.tsx
+++ web/src/pages/GFashion/index.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link } from "react-router-dom";
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
 import MainFrame from '../../components/MainFrame';
+import Slogan from './slogan';
 import { selectRecommendations, fetchRecommendations } from '../../app/slices/recommendationsSlice';
 import { useSelector, useDispatch } from 'react-redux';
 
@@ -20,7 +21,7 @@ const useStyles = makeStyles((theme: Theme) =>
 
 export default () => {
   const [isDataLoaded, setIsDataLoaded] = useState(false);
-  const classes =  useStyles();
+  const classes = useStyles();
   const recommendations = useSelector(selectRecommendations);
   const dispatch = useDispatch();
 
@@ -34,8 +35,7 @@ export default () => {
   return (
     <MainFrame>
       <div className={classes.root}>
-        <h1>Gfashion page</h1>
-        <Link to='/' className={classes.link}>go to home</Link>
+        <Slogan></Slogan>
       </div>
     </MainFrame>
   )
---
+++ web/src/pages/GFashion/slogan.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import { Grid } from '@material-ui/core';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center'
+    },
+    paper: {
+      padding: theme.spacing(2),
+      color: theme.palette.text.secondary,
+      height: 300,
+      minWidth: 800,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center'
+    },
+  }),
+);
+
+export default () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid alignContent='center' alignItems='center'>
+          <Paper className={classes.paper}>Gfashion slogan</Paper>
+        </Grid>
+      </Grid>
+    </div>
+  )
+}
