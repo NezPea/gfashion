3de2490ddfb3c6e90a4a52adcc4834d29ee225d8
grantzou <grantzou.nz@gmail.com>
2020-05-14 15:10:09 +1200

    add starter multi-module structure with h2 in memory auto-configuration

---
+++ g-fashion-api/src/test/java/com/gfashion/api/GfashionDesignerResourceTest.java
@@ -0,0 +1,18 @@
+package com.gfashion.api;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class GfashionDesignerResourceTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
