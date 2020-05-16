9cd5c5f5faa2083490ffd6d31b19c248946295d1
Imtiny <tzhang20200709@gmail.com>
2020-05-16 22:34:50 +0800

    [Home page] implement desiner carousel component

--- web/src/pages/GFashion/index.tsx
+++ web/src/pages/GFashion/index.tsx
@@ -28,6 +28,26 @@ const designers: Array<Designer> = [
   {
     name: 'Trump',
     avatar: ''
+  },
+  {
+    name: 'Miles2',
+    avatar: ''
+  },
+  {
+    name: 'Luther2',
+    avatar: ''
+  },
+  {
+    name: 'Pence2',
+    avatar: ''
+  },
+  {
+    name: 'Ivanka2',
+    avatar: ''
+  },
+  {
+    name: 'Trump2',
+    avatar: ''
   }
 ]
 
