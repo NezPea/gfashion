10144d238c7394333b60ff99cb35e575a2406b25
Imtiny <tzhang20200709@gmail.com>
2020-05-24 10:08:01 +0800

    optimize the banner section

--- web/src/pages/GFashion/Home/gclubGate.tsx
+++ web/src/pages/GFashion/Home/gclubGate.tsx
@@ -1,43 +1,95 @@
 import React from 'react';
 import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Grid, Typography } from '@material-ui/core';
+import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
-      height: 300
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+      [theme.breakpoints.up("xl")]: {
+        height: 946
+      },
+      [theme.breakpoints.up('lg')]: {
+        height: 700
+      },
+      [theme.breakpoints.up("md")]: {
+        height: 500
+      }
     },
-    gateBox: {
-      border: '1px solid #ccc',
-      backgroundColor: theme.palette.background.paper,
-      width: 250,
-      height: 80,
-      margin: '0 15px',
+    sectionTitle: {
+      marginBottom: 50
+    },
+    gates: {
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center'
+    },
+    gateBox: {
+      border: '1px solid #be9c63',
+      [theme.breakpoints.up("xl")]: {
+        width: 440,
+        height: theme.spacing(20) - 5,
+        '& .himaCoin': {
+          fontSize: 40
+        },
+        '& .gDollar': {
+          fontSize: 40
+        }
+      },
+      [theme.breakpoints.up("lg")]: {
+        width: 380,
+        height: theme.spacing(15),
+        '& .himaCoin': {
+          fontSize: 32
+        },
+        '& .gDollar': {
+          fontSize: 32
+        }
+      },
+      [theme.breakpoints.up('md')]: {
+        width: 300,
+        height: theme.spacing(12),
+        '& .himaCoin': {
+          fontSize: 24
+        },
+        '& .gDollar': {
+          fontSize: 24
+        }
+      },
+      margin: '0 15px',
+      padding: theme.spacing(6, 4),
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      backgroundColor: 'rgba(0, 0, 0, 0.65)',
+      color: '#be9c63',
+      '& .himaCoin': {
+        fontFamily: `'STSongti-SC'`
+      },
+      '& .gDollar': {
+        fontFamily: `'Georgia'`
+      }
     }
-  }),
+  })
 );
 
 export default () => {
   const classes = useStyles();
 
   return (
-    <Grid item className={classes.root}>
-      <Grid container spacing={3} direction='column' justify='space-around'>
-        <Grid item xs={12}>
-          <Typography variant='h2' align='center'>Gclub</Typography>
-        </Grid>
-        <Grid container direction='row' alignItems='center' justify='center'>
-          <Grid className={classes.gateBox}>
-            <Typography variant='h6' align='center'>喜马拉雅币</Typography>
-          </Grid>
-          <Grid className={classes.gateBox}>
-            <Typography variant='h6' align='center'>G-dollar</Typography>
-          </Grid>
-        </Grid>
-      </Grid>
-    </Grid>
+    <div className={classes.root}>
+      <Typography variant='h2' align='center' className={classes.sectionTitle}>Join Gclub</Typography>
+      <div className={classes.gates}>
+        <div className={classes.gateBox}>
+          <Typography align='center' className='himaCoin'>Himalaya Coin</Typography>
+        </div>
+        <div className={classes.gateBox}>
+          <Typography align='center' className='gDollar'>G-dollar</Typography>
+        </div>
+      </div>
+    </div>
   )
 }
