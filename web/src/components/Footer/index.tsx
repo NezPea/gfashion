3720fd3ce64777ab50a2e1295380498940962a23
RichardN <richardN@start.com>
2020-05-26 01:49:30 +0800

    update footer style

--- web/src/components/Footer/index.tsx
+++ web/src/components/Footer/index.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import { Link, Typography, List, ListItem } from '@material-ui/core';
+import './index.css';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       width: '100%',
-      backgroundColor: theme.palette.background.paper,
-      marginBottom: 80,
-      padding: '50px 20px'
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+      backgroundColor: theme.palette.background.default,
+      padding: '50px 20px 30px 20px'
     },
     links: {
+      maxWidth: '1400px',
+      width: '100%',
       display: 'flex',
-      alignItems: 'flex-start',
-      justifyContent: 'space-between',
+      justifyContent: 'space-evenly',
       flexDirection: 'row',
       '& .start': {
         display: 'flex',
         flexDirection: 'row'
       }
+    },
+    title: {
+      color: '#222222',
+      fontSize: '14px',
+      fontWeight: 500
+    },
+    content: {
+      paddingTop: '2px',
+      paddingBottom: '2px',
+      color: '#888888',
+      fontSize: '12px',
+      cursor: 'pointer',
+      textDecoration: 'none'
+    },
+    quickLink: {
+      cursor: 'pointer',
+      color: '#be9c63',
+      fontSize: '14px'
     }
   }),
 );
@@ -33,62 +55,62 @@ export default function Footer() {
           <div className="group">
             <List>
               <ListItem>
-                <Typography>联系我们</Typography>
+                <Typography className={classes.title}>联系我们</Typography>
               </ListItem>
               <ListItem>
-                <Typography>vog@gmail.com</Typography>
+                <Typography className={classes.content}>vog@gmail.com</Typography>
               </ListItem>
               <ListItem>
-                <Typography>+1-777-777-777</Typography>
-              </ListItem>
-            </List>
-          </div>
-          <div className="group">
-            <List>
-              <ListItem>
-                <Typography>帮助</Typography>
-              </ListItem>
-              <ListItem>
-                <Link>常见问题</Link>
+                <Typography className={classes.content}>+1-777-777-777</Typography>
               </ListItem>
             </List>
           </div>
           <div className="group">
             <List>
               <ListItem>
-                <Typography>关于我们</Typography>
+                <Typography className={classes.title}>帮助</Typography>
               </ListItem>
               <ListItem>
-                <Link>企业介绍</Link>
-              </ListItem>
-            </List>
-          </div>
-          <div className="group">
-            <List>
-              <ListItem>
-                <Typography>政策与法规</Typography>
-              </ListItem>
-              <ListItem>
-                <Link>隐私协议</Link>
-              </ListItem>
-              <ListItem>
-                <Link>用户协议</Link>
+                <Link className={classes.content}>常见问题</Link>
               </ListItem>
             </List>
           </div>
           <div className="group">
             <List>
               <ListItem>
-                <Typography>语言</Typography>
+                <Typography className={classes.title}>关于我们</Typography>
               </ListItem>
               <ListItem>
-                <Link>中文简体</Link>
+                <Link className={classes.content}>企业介绍</Link>
+              </ListItem>
+            </List>
+          </div>
+          <div className="group">
+            <List>
+              <ListItem>
+                <Typography className={classes.title}>政策与法规</Typography>
               </ListItem>
               <ListItem>
-                <Link>中文繁体</Link>
+                <Link className={classes.content}>隐私协议</Link>
               </ListItem>
               <ListItem>
-                <Link>English</Link>
+                <Link className={classes.content}>用户协议</Link>
+              </ListItem>
+            </List>
+          </div>
+          <div className="group">
+            <List>
+              <ListItem>
+                <Typography className={classes.title}>语言</Typography>
+              </ListItem>
+              <ListItem>
+                <Link className={classes.content}>中文简体</Link>
+              </ListItem>
+              <ListItem>
+                <Link className={classes.content}>中文繁体</Link>
+              </ListItem>
+              <ListItem>
+                <Link className={classes.content}>English</Link>
               </ListItem>
             </List>
           </div>
@@ -96,18 +118,18 @@ export default function Footer() {
         <div className="end">
           <List>
             <ListItem>
-              <Link>加入Gclub</Link>
+              <Link className={classes.quickLink}>加入Gclub</Link>
             </ListItem>
             <ListItem>
-              <Link>入驻Gfashion</Link>
+              <Link className={classes.quickLink}>入驻Gfashion</Link>
             </ListItem>
             <ListItem>
-              <Link>入驻Gmall</Link>
+              <Link className={classes.quickLink}>入驻Gmall</Link>
             </ListItem>
           </List>
         </div>
       </div>
-      <div className="copyright">Copyright © 2020 VOG. All rights reserved.</div>
+      <div className="copyright">Copyright &copy; 2020 VOG. All rights reserved.</div>
     </div>
   )
 }
