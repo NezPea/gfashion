55a3a70bd8bd2bffa1aee275c8315e5d13917996
SamuelCheney <tallsamuelcheney@gmail.com>
2020-05-25 18:41:22 +0700

    picture cover style

--- web/src/pages/GFashion/designer-details-page/components/left-primary-picture-story.tsx
+++ web/src/pages/GFashion/designer-details-page/components/left-primary-picture-story.tsx
@@ -6,22 +6,17 @@ class LeftPrimaryPictureStory extends React.Component {
     render() {
         return (
             <div className="story-container">
-                <div className="primary-picture-column">
-                    {/* <div className="top-spacing-1"></div>
-                    <div className="primary-picture-container">
-                        <img className="primary-picture"></img>
-                    </div> */}
+                <div className="primary-picture-container">
+                    <img className="primary-picture" src='http://localhost:3004/grey-wide.png'></img>
                 </div>
-                {/* <div className="middle-spacing"></div>
                 <div className="text-and-little-pictures">
-                    <div className="top-spacing-2"></div>
+                    {/* <div className="top-spacing-2"></div>
                     <div className="title"></div>
                     <div className="title-description-spacing"></div>
                     <div className="description"></div>
                     <div className="description-little-pictures-spacing"></div>
-                    <div className="little-pictures"></div>
+                    <div className="little-pictures"></div> */}
                 </div>
-                <div className="right-spacing"></div> */}
             </div>
         )
     }
