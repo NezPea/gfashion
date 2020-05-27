31a79e4715cece66b690db2a370f9caed84982b6
SamuelCheney <tallsamuelcheney@gmail.com>
2020-05-27 11:59:30 +0700

    complete designer profile responsive styling

--- web/src/pages/GFashion/designer-details-page/designer-details-page.tsx
+++ web/src/pages/GFashion/designer-details-page/designer-details-page.tsx
@@ -13,6 +13,10 @@ class DesignerDetailsPageDesktop extends React.Component<any, any> {
                     </div>
                     <div className="header"></div>
                     <div className="designer-profile-container">
+                        <div className="designer-profile-background">
+                            <div className="row-1-background"></div>
+                            <div className="row-2-background"></div>
+                        </div>
                         <div className="designer-profile-min-spacing"></div>
                         <div className="designer-profile">
                             <div className="row-1">
@@ -27,15 +31,22 @@ class DesignerDetailsPageDesktop extends React.Component<any, any> {
                             </div>
                             <div className="row-2">
                                 <div className="brands">
-                                    {Array.from(Array(5).keys()).map(_ => (
-                                        <div className="brand">
-                                            <div className="brand-logo-container">
-                                                <img className="brand-logo"></img>
+                                    {Array.from(Array(4).keys()).map(_ => (
+                                        <div className="brand-container">
+                                            <div className="brand">
+                                                <div className="brand-logo-container">
+                                                    <img className="brand-logo"></img>
+                                                </div>
+                                                <div className="brand-name"></div>
                                             </div>
-                                            <div className="brand-name"></div>
                                         </div>
                                     ))}
-                                    <div className="cancel-spacing"></div>
+                                    <div className="brand">
+                                        <div className="brand-logo-container">
+                                            <img className="brand-logo"></img>
+                                        </div>
+                                        <div className="brand-name"></div>
+                                    </div>
                                 </div>
                             </div>
                             <div className="photo-frame">
@@ -45,17 +56,21 @@ class DesignerDetailsPageDesktop extends React.Component<any, any> {
                         <div className="designer-profile-min-spacing"></div>
                     </div>
                 </div>
-                {/* <div className="content">
-                    <div className="designer-stories">
-                        <div className="designer-story-container">
-                            <div>
-                                <LeftPrimaryPictureStory />
+                <div className="content-container">
+                    <div className="content-spacing"></div>
+                    <div className="content">
+                        {/* <div className="designer-stories">
+                            <div className="designer-story-container">
+                                <div>
+                                    <LeftPrimaryPictureStory />
+                                </div>
                             </div>
                         </div>
+                        <div className="featured-products"></div> */}
                     </div>
-                    <div className="featured-products"></div>
+                    <div className="content-spacing"></div>
                 </div>
-                <div className="footer"></div> */}
+                {/* <div className="footer"></div> */}
             </div>
         )
     }
