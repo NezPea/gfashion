aa0bc729297cb37c3dc4d9f7d48f238969f1d62e
Imtiny <tzhang20200709@gmail.com>
2020-05-30 23:06:14 +0800

    add follow brands and designers

--- web/src/pages/GFashion/Home/designerCarousel.tsx
+++ web/src/pages/GFashion/Home/designerCarousel.tsx
@@ -1,29 +1,41 @@
-import React from 'react';
-import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
-import { Typography, Box, Button } from '@material-ui/core';
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
-import Avatar from '@material-ui/core/Avatar';
-import { ChevronLeft, ChevronRight } from '@material-ui/icons';
-import { DesignersProps } from '../../../app/types';
-import { useTranslation } from 'react-i18next';
-import { I18N, I18N_NS } from './_i18n';
+import React from 'react'
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles'
+import { Typography, Box, Button } from '@material-ui/core'
+import {
+  CarouselProvider,
+  Slider,
+  Slide,
+  ButtonBack,
+  ButtonNext
+} from 'pure-react-carousel'
+import Avatar from '@material-ui/core/Avatar'
+import { ChevronLeft, ChevronRight } from '@material-ui/icons'
+import {
+  DesignersProps,
+  HomepageDesigner,
+  FollowingDesignersMap
+} from '../../../app/types'
+import { useTranslation } from 'react-i18next'
+import { I18N, I18N_NS } from './_i18n'
 
-import 'pure-react-carousel/dist/react-carousel.es.css';
+import 'pure-react-carousel/dist/react-carousel.es.css'
+import { doFollowDesigner } from 'src/app/slices/homeRecommendationsSlice'
+import { useDispatch } from 'react-redux'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       overflow: 'hidden',
       backgroundColor: theme.palette.background.paper,
-      [theme.breakpoints.down("xl")]: {
+      [theme.breakpoints.down('xl')]: {
         padding: theme.spacing(20, 0),
         height: 825
       },
-      [theme.breakpoints.down("lg")]: {
+      [theme.breakpoints.down('lg')]: {
         padding: theme.spacing(16, 0),
         height: 680
       },
-      [theme.breakpoints.down("md")]: {
+      [theme.breakpoints.down('md')]: {
         padding: theme.spacing(12, 0),
         height: 580
       }
@@ -125,30 +137,63 @@ const useStyles = makeStyles((theme: Theme) =>
         cursor: `default`
       },
       '&:hover .action-follow': {
-        visibility: 'visible',
+        visibility: 'visible'
       },
       '&:hover .avatar': {
         filter: `none`
       }
     }
-  }),
-);
+  })
+)
 
-export const DesignerCarousel: React.FunctionComponent<DesignersProps> = ({ designers = [] }) => {
-  const classes = useStyles();
-  const { t } = useTranslation(I18N_NS);
+export const DesignerCarousel: React.FunctionComponent<DesignersProps> = ({
+  designers = [],
+  followingDesigners = []
+}) => {
+  const classes = useStyles()
+  const { t } = useTranslation(I18N_NS)
+  const dispatch = useDispatch()
+
+  const followingDesignersMap: FollowingDesignersMap = followingDesigners.reduce(
+    (acc: FollowingDesignersMap, cur: HomepageDesigner) => {
+      acc[`${cur.id}`] = cur
+      return acc
+    },
+    {}
+  )
+
+  const follow = (brand: HomepageDesigner) => {
+    return () => {
+      dispatch(doFollowDesigner(brand))
+    }
+  }
 
   const buildSlides = () => {
     return designers.map((m, i) => {
+      const isFollowing: Boolean = !!followingDesignersMap[`${m.id}`]
       return (
         <Slide key={i} index={i} className={classes.slide}>
           <Box className={classes.slideBox}>
-            <Avatar src={require(`../../../assets/images/designer${i % 6 + 1}.png`)} variant='rounded' className={`${classes.avatar} avatar`} />
-            <div className='designer-info'>
-              <Typography className='designer-name'>{m.name}</Typography>
-              <Typography className='designer-brand'>{m.cooperatingBrands[0]}</Typography>
+            <Avatar
+              src={require(`../../../assets/images/designer${(i % 6) + 1}.png`)}
+              variant="rounded"
+              className={`${classes.avatar} avatar`}
+            />
+            <div className="designer-info">
+              <Typography className="designer-name">{m.name}</Typography>
+              <Typography className="designer-brand">
+                {m.cooperatingBrands[0]}
+              </Typography>
             </div>
-            <Button variant='contained' color='secondary' className='action-follow'>{t(I18N._common.follow_button_text)}</Button>
+            <Button
+              variant="contained"
+              color="secondary"
+              className="action-follow"
+              onClick={follow(m)}>
+              {isFollowing
+                ? t(I18N._common.following_button_text)
+                : t(I18N._common.follow_button_text)}
+            </Button>
           </Box>
         </Slide>
       )
@@ -159,26 +204,29 @@ export const DesignerCarousel: React.FunctionComponent<DesignersProps> = ({ desi
     <div className={classes.root}>
       <div className={classes.headline}>
         <div className={classes.surroundingLine}></div>
-        <Typography className={classes.sectionTitle} align='center'>
+        <Typography className={classes.sectionTitle} align="center">
           {t(I18N.home.recommended_designers.title)}
         </Typography>
         <div className={classes.surroundingLine}></div>
       </div>
-      <Typography className={classes.sectionDescription}>{t(I18N.home.recommended_designers.description)}</Typography>
+      <Typography className={classes.sectionDescription}>
+        {t(I18N.home.recommended_designers.description)}
+      </Typography>
       <CarouselProvider
         naturalSlideWidth={320}
         naturalSlideHeight={320}
         totalSlides={designers ? designers.length : 5}
         visibleSlides={6}
         orientation="horizontal"
-        className={classes.carouselProvider}
-      >
-        <Slider style={{ overflow: 'visible' }}>
-          {buildSlides()}
-        </Slider>
-        <ButtonBack className={`${classes.slideButton} prev`}><ChevronLeft /></ButtonBack>
-        <ButtonNext className={`${classes.slideButton} next`}><ChevronRight /></ButtonNext>
+        className={classes.carouselProvider}>
+        <Slider style={{ overflow: 'visible' }}>{buildSlides()}</Slider>
+        <ButtonBack className={`${classes.slideButton} prev`}>
+          <ChevronLeft />
+        </ButtonBack>
+        <ButtonNext className={`${classes.slideButton} next`}>
+          <ChevronRight />
+        </ButtonNext>
       </CarouselProvider>
-    </div >
+    </div>
   )
 }
--- web/src/pages/GFashion/Home/index.tsx
+++ web/src/pages/GFashion/Home/index.tsx
@@ -1,25 +1,30 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import MainFrame from '../../../components/MainFrame';
-import { DesignerCarousel } from './designerCarousel';
-import GclubGate from './gclubGate';
-import { Brands } from './brands';
-import { RecommendedProducts } from './recommendedProducts';
-import { selectHomeRecommendations, fetchHomeRecommendations } from '../../../app/slices/homeRecommendationsSlice';
-import { Helmet } from 'react-helmet-async';
-import { useTranslation } from 'react-i18next';
-import { I18N, I18N_NS } from './_i18n';
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import CircularProgress from '@material-ui/core/CircularProgress'
+import MainFrame from '../../../components/MainFrame'
+import { DesignerCarousel } from './designerCarousel'
+import GclubGate from './gclubGate'
+import { Brands } from './brands'
+import { RecommendedProducts } from './recommendedProducts'
+import {
+  selectHomeRecommendations,
+  fetchHomeRecommendations
+} from '../../../app/slices/homeRecommendationsSlice'
+import { Helmet } from 'react-helmet-async'
+import { useTranslation } from 'react-i18next'
+import { I18N, I18N_NS } from './_i18n'
 
 export default () => {
-  const home = useSelector(selectHomeRecommendations);
-  const dispatch = useDispatch();
+  const home = useSelector(selectHomeRecommendations)
+  const dispatch = useDispatch()
   const { t } = useTranslation(I18N_NS)
 
   useEffect(() => {
-    dispatch(fetchHomeRecommendations({
-      url: '/homepage'
-    }))
+    dispatch(
+      fetchHomeRecommendations({
+        url: '/homepage'
+      })
+    )
   }, [dispatch])
 
   return (
@@ -27,15 +32,23 @@ export default () => {
       <Helmet>
         <title>{t(I18N.home._self)}</title>
       </Helmet>
-      {
-        home.isLoading ? <CircularProgress color="secondary" /> :
-          <React.Fragment>
-            <GclubGate />
-            <DesignerCarousel designers={home.recommendations?.recommendedDesigners} />
-            <Brands brands={home.recommendations?.recommendedBrands}></Brands>
-            <RecommendedProducts products={home.recommendations?.recommendedProducts} />
-          </React.Fragment>
-      }
+      {home.isLoading ? (
+        <CircularProgress color="secondary" />
+      ) : (
+        <React.Fragment>
+          <GclubGate />
+          <DesignerCarousel
+            designers={home.recommendations?.recommendedDesigners}
+            followingDesigners={home.recommendations?.followingDesigners}
+          />
+          <Brands
+            brands={home.recommendations?.recommendedBrands}
+            followingBrands={home.recommendations?.followingBrands}></Brands>
+          <RecommendedProducts
+            products={home.recommendations?.recommendedProducts}
+          />
+        </React.Fragment>
+      )}
     </MainFrame>
   )
 }
