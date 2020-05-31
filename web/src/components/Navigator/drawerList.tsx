27d27b2847920caeff752d1cf2b6d6d6a6f7ca47
Imtiny <tzhang20200709@gmail.com>
2020-05-31 17:44:39 +0800

    adjust the styles of gfashion list

--- web/src/components/Navigator/drawerList.tsx
+++ web/src/components/Navigator/drawerList.tsx
@@ -1,137 +1,181 @@
-import React from 'react';
-import { List, Divider, ListItem, ListItemIcon, ListItemText, Collapse } from '@material-ui/core'
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import { ExpandLess, ExpandMore, StarBorder } from '@material-ui/icons';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import React from 'react'
+import {
+  List,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Collapse,
+  Avatar
+} from '@material-ui/core'
+import InboxIcon from '@material-ui/icons/MoveToInbox'
+import MailIcon from '@material-ui/icons/Mail'
+import { Add, Remove } from '@material-ui/icons'
+import { makeStyles, Theme, createStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     list: {
-      width: 250,
+      [theme.breakpoints.down('xl')]: {
+        width: 464
+      },
+      [theme.breakpoints.down('lg')]: {
+        width: 350
+      }
     },
-    nested: {
-      paddingLeft: theme.spacing(4),
+    nestedSecond: {
+      paddingLeft: theme.spacing(10)
+    },
+    nestedThird: {
+      paddingLeft: theme.spacing(12)
+    },
+    mainListItemIcon: {
+      width: 18,
+      height: 18
+    },
+    followingItem: {
+      minWidth: 35,
+      marginLeft: -35,
+      '& .icon': {
+        width: 20,
+        height: 19
+      }
+    },
+    mainListItem: {
+      '&.open': {
+        backgroundColor: '#2C2C2C',
+        color: theme.palette.secondary.main
+      }
     }
-  }),
-);
+  })
+)
 
 export default function DrawerList() {
-  const [open, setOpen] = React.useState(true);
-  const [gfWomenOpen, setGfWomenOpen] = React.useState(false);
-  const [gfMenOpen, setGfMenOpen] = React.useState(false);
-  const [gfOthersOpen, setGfOthersOpen] = React.useState(true);
-  const [gfFollowingOpen, setGfFollowingOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(true)
+  const [gfWomenOpen, setGfWomenOpen] = React.useState(false)
+  const [gfMenOpen, setGfMenOpen] = React.useState(false)
+  const [gfOthersOpen, setGfOthersOpen] = React.useState(true)
+  const [gfFollowingOpen, setGfFollowingOpen] = React.useState(true)
 
   const classes = useStyles()
 
   return (
-    <div
-      className={classes.list}
-      role="presentation"
-    >
+    <div className={classes.list} role="presentation">
       <List>
         {['Himalaya Coin', 'G-Dollar', 'Gclub'].map((text, index) => (
           <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
         ))}
       </List>
       <Divider />
       <List>
-        <ListItem button key={'Gfashion'} onClick={() => { setOpen(!open) }}>
-          <ListItemIcon><InboxIcon /></ListItemIcon>
+        <ListItem
+          button
+          key={'Gfashion'}
+          className={`${classes.mainListItem} ${open ? 'open' : ''}`}
+          onClick={() => {
+            setOpen(!open)
+          }}>
+          <ListItemIcon>
+            <Avatar
+              className={classes.mainListItemIcon}
+              src={require(`../../assets/images/gfashion_icon_${
+                open ? '' : 'in'
+              }active.svg`)}
+            />
+          </ListItemIcon>
           <ListItemText primary={'Gfashion'} />
-          {open ? <ExpandLess /> : <ExpandMore />}
         </ListItem>
         <Collapse in={open} timeout="auto" unmountOnExit>
           <List component="div" disablePadding>
-            <ListItem button className={classes.nested} onClick={() => { setGfWomenOpen(!gfWomenOpen) }}>
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
+            <ListItem
+              button
+              className={classes.nestedSecond}
+              onClick={() => {
+                setGfWomenOpen(!gfWomenOpen)
+              }}>
               <ListItemText primary="Women" />
+              {gfWomenOpen ? <Remove /> : <Add />}
             </ListItem>
             <Collapse in={gfWomenOpen} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
-                <ListItem button className={classes.nested}>
-                  <ListItemIcon>
-                    <StarBorder />
-                  </ListItemIcon>
-                  <ListItemText secondary="Women" />
+                <ListItem button className={classes.nestedThird}>
+                  <ListItemText secondary="Skirts" />
                 </ListItem>
-                <ListItem button className={classes.nested}>
-                  <ListItemIcon>
-                    <StarBorder />
-                  </ListItemIcon>
-                  <ListItemText secondary="Women" />
+                <ListItem button className={classes.nestedThird}>
+                  <ListItemText secondary="Pants" />
                 </ListItem>
               </List>
             </Collapse>
-            <ListItem button className={classes.nested} onClick={() => { setGfMenOpen(!gfMenOpen) }}>
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
+            <ListItem
+              button
+              className={classes.nestedSecond}
+              onClick={() => {
+                setGfMenOpen(!gfMenOpen)
+              }}>
               <ListItemText primary="Men" />
+              {gfMenOpen ? <Remove /> : <Add />}
             </ListItem>
             <Collapse in={gfMenOpen} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
-                <ListItem button className={classes.nested}>
-                  <ListItemIcon>
-                    <StarBorder />
-                  </ListItemIcon>
-                  <ListItemText secondary="Women" />
+                <ListItem button className={classes.nestedThird}>
+                  <ListItemText secondary="Shoes" />
                 </ListItem>
-                <ListItem button className={classes.nested}>
-                  <ListItemIcon>
-                    <StarBorder />
-                  </ListItemIcon>
-                  <ListItemText secondary="Women" />
+                <ListItem button className={classes.nestedThird}>
+                  <ListItemText secondary="T-shirts" />
                 </ListItem>
               </List>
             </Collapse>
-            <ListItem button className={classes.nested} onClick={() => { setGfOthersOpen(!gfOthersOpen) }}>
-              <ListItemIcon>
-                <StarBorder />
-              </ListItemIcon>
+            <ListItem
+              button
+              className={classes.nestedSecond}
+              onClick={() => {
+                setGfOthersOpen(!gfOthersOpen)
+              }}>
               <ListItemText primary="Others" />
+              {gfOthersOpen ? <Remove /> : <Add />}
             </ListItem>
             <Collapse in={gfOthersOpen} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
-                <ListItem button className={classes.nested}>
-                  <ListItemIcon>
-                    <StarBorder />
-                  </ListItemIcon>
-                  <ListItemText secondary="Women" />
+                <ListItem button className={classes.nestedThird}>
+                  <ListItemText secondary="HOME" />
                 </ListItem>
-                <ListItem button className={classes.nested}>
-                  <ListItemIcon>
-                    <StarBorder />
-                  </ListItemIcon>
-                  <ListItemText secondary="Women" />
+                <ListItem button className={classes.nestedThird}>
+                  <ListItemText secondary="ART" />
+                </ListItem>
+                <ListItem button className={classes.nestedThird}>
+                  <ListItemText secondary="TECHNOLOGY" />
+                </ListItem>
+                <ListItem button className={classes.nestedThird}>
+                  <ListItemText secondary="CULTURE" />
                 </ListItem>
               </List>
             </Collapse>
             <Divider />
-            <ListItem button className={classes.nested} onClick={() => { setGfFollowingOpen(!gfFollowingOpen) }}>
-              <ListItemIcon>
-                <StarBorder />
+            <ListItem
+              button
+              className={classes.nestedSecond}
+              onClick={() => {
+                setGfFollowingOpen(!gfFollowingOpen)
+              }}>
+              <ListItemIcon className={classes.followingItem}>
+                <Avatar
+                  className="icon"
+                  src={require(`../../assets/images/following_icon.svg`)}
+                />
               </ListItemIcon>
               <ListItemText primary="Following" />
             </ListItem>
             <Collapse in={gfFollowingOpen} timeout="auto" unmountOnExit>
               <List component="div" disablePadding>
-                <ListItem button className={classes.nested}>
-                  <ListItemIcon>
-                    <StarBorder />
-                  </ListItemIcon>
+                <ListItem button className={classes.nestedThird}>
                   <ListItemText secondary="Women" />
                 </ListItem>
-                <ListItem button className={classes.nested}>
-                  <ListItemIcon>
-                    <StarBorder />
-                  </ListItemIcon>
+                <ListItem button className={classes.nestedThird}>
                   <ListItemText secondary="Women" />
                 </ListItem>
               </List>
@@ -143,7 +187,9 @@ export default function DrawerList() {
       <List>
         {['Gmail', 'GTV', 'Gnews'].map((text, index) => (
           <ListItem button key={text}>
-            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+            <ListItemIcon>
+              {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+            </ListItemIcon>
             <ListItemText primary={text} />
           </ListItem>
         ))}
