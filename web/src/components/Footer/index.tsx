ecdc466680f39f49512451b0bda52057ff2a4054
SamuelCheney <tallsamuelcheney@gmail.com>
2020-05-31 13:25:01 +0700

    convert footer to class component

--- web/src/components/Footer/index.tsx
+++ web/src/components/Footer/index.tsx
@@ -1,9 +1,102 @@
 import React from 'react';
-import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
+import { withStyles, createStyles, Theme } from '@material-ui/core/styles';
 import { Link, Typography, List, ListItem } from '@material-ui/core';
-import './index.css';
 
-const useStyles = makeStyles((theme: Theme) =>
+class _Footer extends React.Component<any> {
+
+  render() {
+
+    const { classes } = this.props
+
+    return (
+      <div className={classes.root}>
+        <div className={classes.links}>
+          <div className="start">
+            <div className="group">
+              <List>
+                <ListItem>
+                  <Typography className={classes.title}>联系我们</Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography className={classes.content}>vog@gmail.com</Typography>
+                </ListItem>
+                <ListItem>
+                  <Typography className={classes.content}>+1-777-777-777</Typography>
+                </ListItem>
+              </List>
+            </div>
+            <div className="group">
+              <List>
+                <ListItem>
+                  <Typography className={classes.title}>帮助</Typography>
+                </ListItem>
+                <ListItem>
+                  <Link className={classes.content}>常见问题</Link>
+                </ListItem>
+              </List>
+            </div>
+            <div className="group">
+              <List>
+                <ListItem>
+                  <Typography className={classes.title}>关于我们</Typography>
+                </ListItem>
+                <ListItem>
+                  <Link className={classes.content}>企业介绍</Link>
+                </ListItem>
+              </List>
+            </div>
+            <div className="group">
+              <List>
+                <ListItem>
+                  <Typography className={classes.title}>政策与法规</Typography>
+                </ListItem>
+                <ListItem>
+                  <Link className={classes.content}>隐私协议</Link>
+                </ListItem>
+                <ListItem>
+                  <Link className={classes.content}>用户协议</Link>
+                </ListItem>
+              </List>
+            </div>
+            <div className="group">
+              <List>
+                <ListItem>
+                  <Typography className={classes.title}>语言</Typography>
+                </ListItem>
+                <ListItem>
+                  <Link className={classes.content}>中文简体</Link>
+                </ListItem>
+                <ListItem>
+                  <Link className={classes.content}>中文繁体</Link>
+                </ListItem>
+                <ListItem>
+                  <Link className={classes.content}>English</Link>
+                </ListItem>
+              </List>
+            </div>
+          </div>
+          <div className="end">
+            <List>
+              <ListItem>
+                <Link className={classes.quickLink}>加入Gclub</Link>
+              </ListItem>
+              <ListItem>
+                <Link className={classes.quickLink}>入驻Gfashion</Link>
+              </ListItem>
+              <ListItem>
+                <Link className={classes.quickLink}>入驻Gmall</Link>
+              </ListItem>
+            </List>
+          </div>
+        </div>
+        <div className="copyright">Copyright &copy; 2020 VOG. All rights reserved.</div>
+      </div>
+    )
+
+  }
+}
+
+const Footer = withStyles((theme: Theme) =>
   createStyles({
     root: {
       width: '100%',
@@ -11,7 +104,23 @@ const useStyles = makeStyles((theme: Theme) =>
       flexDirection: 'column',
       alignItems: 'center',
       backgroundColor: theme.palette.background.default,
-      padding: '50px 20px 30px 20px'
+      padding: '50px 20px 30px 20px',
+      '& .group': {
+        marginRight: '4rem',
+        fontFamily: 'PingFangSC'
+      },
+      '& .group:last-child': {
+        marginRight: '0'
+      },
+      '& .end': {
+        fontFamily: "'PingFangSC'"
+      },
+      '& .copyright': {
+        marginTop: '50px',
+        fontFamily: "'PingFangSC'",
+        fontSize: '12px',
+        color: '#888888'
+      }
     },
     links: {
       maxWidth: '1400px',
@@ -43,93 +152,9 @@ const useStyles = makeStyles((theme: Theme) =>
       fontSize: '14px'
     }
   }),
-);
+  {
+    withTheme: true
+  }
+)(_Footer);
 
-export default function Footer() {
-  const classes = useStyles()
-
-  return (
-    <div className={classes.root}>
-      <div className={classes.links}>
-        <div className="start">
-          <div className="group">
-            <List>
-              <ListItem>
-                <Typography className={classes.title}>联系我们</Typography>
-              </ListItem>
-              <ListItem>
-                <Typography className={classes.content}>vog@gmail.com</Typography>
-              </ListItem>
-              <ListItem>
-                <Typography className={classes.content}>+1-777-777-777</Typography>
-              </ListItem>
-            </List>
-          </div>
-          <div className="group">
-            <List>
-              <ListItem>
-                <Typography className={classes.title}>帮助</Typography>
-              </ListItem>
-              <ListItem>
-                <Link className={classes.content}>常见问题</Link>
-              </ListItem>
-            </List>
-          </div>
-          <div className="group">
-            <List>
-              <ListItem>
-                <Typography className={classes.title}>关于我们</Typography>
-              </ListItem>
-              <ListItem>
-                <Link className={classes.content}>企业介绍</Link>
-              </ListItem>
-            </List>
-          </div>
-          <div className="group">
-            <List>
-              <ListItem>
-                <Typography className={classes.title}>政策与法规</Typography>
-              </ListItem>
-              <ListItem>
-                <Link className={classes.content}>隐私协议</Link>
-              </ListItem>
-              <ListItem>
-                <Link className={classes.content}>用户协议</Link>
-              </ListItem>
-            </List>
-          </div>
-          <div className="group">
-            <List>
-              <ListItem>
-                <Typography className={classes.title}>语言</Typography>
-              </ListItem>
-              <ListItem>
-                <Link className={classes.content}>中文简体</Link>
-              </ListItem>
-              <ListItem>
-                <Link className={classes.content}>中文繁体</Link>
-              </ListItem>
-              <ListItem>
-                <Link className={classes.content}>English</Link>
-              </ListItem>
-            </List>
-          </div>
-        </div>
-        <div className="end">
-          <List>
-            <ListItem>
-              <Link className={classes.quickLink}>加入Gclub</Link>
-            </ListItem>
-            <ListItem>
-              <Link className={classes.quickLink}>入驻Gfashion</Link>
-            </ListItem>
-            <ListItem>
-              <Link className={classes.quickLink}>入驻Gmall</Link>
-            </ListItem>
-          </List>
-        </div>
-      </div>
-      <div className="copyright">Copyright &copy; 2020 VOG. All rights reserved.</div>
-    </div>
-  )
-}
+export default Footer
