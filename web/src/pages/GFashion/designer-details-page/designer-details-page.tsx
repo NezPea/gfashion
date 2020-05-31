bbb7cb3398a3da053f23c6c8f8616cf3c10b6f46
SamuelCheney <tallsamuelcheney@gmail.com>
2020-05-31 12:27:14 +0700

    convert designer page to css in js

--- web/src/pages/GFashion/designer-details-page/designer-details-page.tsx
+++ web/src/pages/GFashion/designer-details-page/designer-details-page.tsx
@@ -1,100 +1,323 @@
-import React from 'react';
-import './designer-details-page.css'
-import LeftPrimaryPictureStory from './components/left-primary-picture-story';
-import LeftRectanglePrimaryPictureStory from './components/left-rectangle-primary-picture-story';
-import OneMediaStory from './components/one-media-story';
-import RightPrimaryPictureStory from './components/right-primary-picture-story';
-import Header from './components/header';
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import LeftPrimaryPictureStory from './components/left-primary-picture-story'
+import LeftRectanglePrimaryPictureStory from './components/left-rectangle-primary-picture-story'
+import OneMediaStory from './components/one-media-story'
+import RightPrimaryPictureStory from './components/right-primary-picture-story'
+import Header from './components/header'
 
-class DesignerDetailsPageDesktop extends React.Component<any, any> {
+class _DesignerDetailsPageDesktop extends React.Component<any> {
 
     render() {
+
+        const { classes } = this.props
+
         return (
-            <div className="designer-details-page-1">
-                <div className="top-1">
-                    <div className="background-container-1">
-                        <img className="background-picture-1" alt="one"></img>
+            <div className={classes['designer-details-page']}>
+                <div className='top'>
+                    <div className='background-container'>
+                        <img className='background-picture' alt='one'></img>
                     </div>
-                    <div className="header-1">
+                    <div className='header'>
                         <Header />
                     </div>
-                    <div className="designer-profile-container-1">
-                        <div className="designer-profile-background-1">
-                            <div className="row-1-background-1"></div>
-                            <div className="row-2-background-1"></div>
+                    <div className='designer-profile-container'>
+                        <div className='designer-profile-background'>
+                            <div className='row-1-background'></div>
+                            <div className='row-2-background'></div>
                         </div>
-                        <div className="designer-profile-min-spacing-1"></div>
-                        <div className="designer-profile-second-container-1">
-                            <div className="designer-profile-1">
-                                <div className="row-1-1">
-                                    <div className="photo-space-1"></div>
-                                    <div className="text-profile-1">
-                                        <div className="designer-name-1">Pierre Hardy</div>
-                                        <div className="space-between-1"></div>
-                                        <div className="designer-description-1">
+                        <div className='designer-profile-min-spacing'></div>
+                        <div className='designer-profile-second-container'>
+                            <div className='designer-profile'>
+                                <div className='row-1'>
+                                    <div className='photo-space'></div>
+                                    <div className='text-profile'>
+                                        <div className='designer-name'>Pierre Hardy</div>
+                                        <div className='space-between'></div>
+                                        <div className='designer-description'>
                                             {'Turn to covetable Balmain womenswear for a vampish approach to feminine glamour. Military-inspired tailoring'}
                                             {' lends instant impact to tuxedo dresses and mini skirts, alongside signature blazer jackets.'}
                                         </div>
                                     </div>
                                 </div>
-                                <div className="row-2-1">
-                                    <div className="photo-space-1"></div>
-                                    <div className="brands-1">
+                                <div className='row-2'>
+                                    <div className='photo-space'></div>
+                                    <div className='brands'>
                                         {
                                             Array.from(Array(4).keys()).map(_ => (
-                                                <div className="brand-container-with-margin-right-1">
-                                                    <div className="brand-1">
-                                                        <div className="brand-logo-container-1">
-                                                            <img className="brand-logo-1" alt="one"></img>
+                                                <div className='brand-container-with-margin-right'>
+                                                    <div className='brand'>
+                                                        <div className='brand-logo-container'>
+                                                            <img className='brand-logo' alt='one'></img>
                                                         </div>
-                                                        <div className="brand-name-1"></div>
+                                                        <div className='brand-name'></div>
                                                     </div>
                                                 </div>
                                             ))
                                         }
-                                        <div className="brand-container-without-margin-right-1">
-                                            <div className="brand-1">
-                                                <div className="brand-logo-container-1">
-                                                    <img className="brand-logo-1" alt="one"></img>
+                                        <div className='brand-container-without-margin-right'>
+                                            <div className='brand'>
+                                                <div className='brand-logo-container'>
+                                                    <img className='brand-logo' alt='one'></img>
                                                 </div>
-                                                <div className="brand-name-1"></div>
+                                                <div className='brand-name'></div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
-                                <div className="photo-frame-1">
-                                    <img className="photo-1" alt="one"></img>
+                                <div className='photo-frame'>
+                                    <img className='photo' alt='one'></img>
                                 </div>
                             </div>
                         </div>
-                        <div className="designer-profile-min-spacing-1"></div>
+                        <div className='designer-profile-min-spacing'></div>
                     </div>
                 </div>
-                <div className="content-container-1">
-                    <div className="content-spacing-1"></div>
-                    <div className="content-1">
-                        <div className="designer-stories-1">
-                            <div className="designer-story-container-1">
+                <div className='content-container'>
+                    <div className='content-spacing'></div>
+                    <div className='content'>
+                        <div className='designer-stories'>
+                            <div className='designer-story-container'>
                                 <LeftPrimaryPictureStory />
                             </div>
-                            <div className="designer-story-container-1">
+                            <div className='designer-story-container'>
                                 <LeftRectanglePrimaryPictureStory />
                             </div>
-                            <div className="designer-story-container-1">
+                            <div className='designer-story-container'>
                                 <OneMediaStory />
                             </div>
-                            <div className="designer-story-container-1">
+                            <div className='designer-story-container'>
                                 <RightPrimaryPictureStory />
                             </div>
                         </div>
-                        {/* <div className="featured-products-1"></div> */}
+                        {/* <div className='featured-products'></div> */}
                     </div>
-                    <div className="content-spacing-1"></div>
+                    <div className='content-spacing'></div>
                 </div>
-                {/* <div className="footer-1"></div> */}
+                {/* <div className='footer'></div> */}
             </div>
         )
     }
 }
+
+const DesignerDetailsPageDesktop = withStyles({
+    'designer-details-page': {
+        position: 'relative',
+        width: '100vm',
+        backgroundColor: '#a0a0a0',
+        '& .top': {
+            position: 'relative',
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'stretch',
+            '& .background-container': {
+                position: 'absolute',
+                top: '0',
+                bottom: '0',
+                left: '0',
+                right: '0',
+            },
+            '& .header': {
+                position: 'relative',
+                height: '5.124rem',
+            },
+            '& .designer-profile-container': {
+                position: 'relative',
+                display: 'flex',
+                width: '100vw',
+                backgroundColor: '#c0c0c0',
+                '& .designer-profile-background': {
+                    position: 'absolute',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    top: '0',
+                    bottom: '0',
+                    width: '100vw',
+                    '& .row-1-background': {
+                        position: 'relative',
+                        height: '21.504rem',
+                    },
+                    '& .row-2-background': {
+                        position: 'relative',
+                        flexGrow: 1,
+                        flexShrink: 0,
+                        height: '12.000rem',
+                        backgroundColor: '#909090',
+                    },
+                },
+                '& .designer-profile-min-spacing': {
+                    position: 'relative',
+                    flexGrow: 1,
+                    flexShrink: 0,
+                    width: '1.000rem',
+                },
+                '& .designer-profile-second-container': {
+                    position: 'relative',
+                    display: 'flex',
+                    flexShrink: 1,
+                    width: '92.875rem',
+                    '& .designer-profile': {
+                        position: 'relative',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'stretch',
+                        flexGrow: 1,
+                        flexShrink: 0,
+                        width: '46.438rem',
+                        maxWidth: '61.062rem',
+                        '& .row-1': {
+                            position: 'relative',
+                            display: 'flex',
+                            height: '21.504rem',
+                            backgroundColor: '#909090',
+                            '& .photo-space': {
+                                position: 'relative',
+                                width: '17.250rem',
+                            },
+                            '& .text-profile': {
+                                position: 'relative',
+                                display: 'flex',
+                                flexDirection: 'column',
+                                alignItems: 'stretch',
+                                marginTop: '12.120rem',
+                                width: '0',
+                                height: '6.936rem',
+                                flexGrow: 1,
+                                flexShrink: 0,
+                                backgroundColor: '#404040',
+                                '& .designer-name': {
+                                    position: 'relative',
+                                    height: '2.500rem',
+                                    fontFamily: 'Montserrat',
+                                    fontSize: '2.496rem',
+                                    fontWeight: 'bold',
+                                    fontStretch: 'normal',
+                                    fontStyle: 'normal',
+                                    lineHeight: '1',
+                                    letterSpacing: 'normal',
+                                    color: '#ffffff',
+                                },
+                                '& .space-between': {
+                                    position: 'relative',
+                                    flexGrow: 1,
+                                    flexShrink: 0,
+                                    backgroundColor: '#707070',
+                                },
+                                '& .designer-description': {
+                                    position: 'relative',
+                                    height: '3.125rem',
+                                    fontFamily: 'SFUIDisplay',
+                                    fontSize: '0.936rem',
+                                    fontWeight: 500,
+                                    fontStretch: 'normal',
+                                    fontStyle: 'normal',
+                                    lineHeight: '1.47',
+                                    letterSpacing: 'normal',
+                                    color: '#ffffff',
+                                    overflow: 'hidden',
+                                },
+                            },
+                        },
+                        '& .row-2': {
+                            position: 'relative',
+                            display: 'flex',
+                            '& .photo-space': {
+                                position: 'relative',
+                                width: '17.250rem',
+                            },
+                            '& .brands': {
+                                position: 'relative',
+                                display: 'flex',
+                                width: '0',
+                                flexGrow: 1,
+                                flexShrink: 0,
+                                flexWrap: 'wrap',
+                                backgroundColor: '#707070',
+                                '& .brand-container-with-margin-right': {
+                                    position: 'relative',
+                                    marginRight: '3.563rem',
+                                },
+                                '& .brand-container-without-margin-right': {
+                                    position: 'relative',
+                                },
+                                '& .brand': {
+                                    position: 'relative',
+                                    paddingTop: '2.652rem',
+                                    paddingBottom: '1.875rem',
+                                    width: '5.256rem',
+                                    backgroundColor: '#606060',
+                                    '& .brand-logo-container': {
+                                        position: 'relative',
+                                        marginBottom: '0.875rem',
+                                        width: '5.256rem',
+                                        height: '5.256rem',
+                                        backgroundColor: '#404040',
+                                        borderRadius: '100%',
+                                    },
+                                    '& .brand-name': {
+                                        position: 'relative',
+                                        width: '5.256rem',
+                                        height: '2.304rem',
+                                        backgroundColor: '#303030',
+                                    },
+                                },
+                            },
+                        },
+                        '& .photo-frame': {
+                            position: 'absolute',
+                            top: '9.624rem',
+                            left: '0rem',
+                            width: '15.000rem',
+                            height: '15.000rem',
+                            backgroundColor: '#404040',
+                            borderRadius: '100%',
+                        },
+                    },
+                }
+            },
+        },
+        '& .content-container': {
+            position: 'relative',
+            display: 'flex',
+            alignItems: 'stretch',
+            '& .content-spacing': {
+                position: 'relative',
+                flexGrow: 1,
+                flexShrink: 0,
+                width: '3.750rem',
+            },
+            '& .content': {
+                position: 'relative',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'stretch',
+                flexShrink: 1,
+                width: '87.500rem',
+                '& .designer-stories': {
+                    position: 'relative',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'stretch',
+                    '& .designer-story-container': {
+                        position: 'relative',
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'stretch',
+                        marginTop: '4.992rem',
+                        backgroundColor: '#909090',
+                    },
+                },
+            },
+        },
+        '& .featured-products': {
+            position: 'relative',
+            height: '699px',
+        },
+        '& .footer': {
+            position: 'relative',
+            height: '298px',
+        },
+    }
+})(_DesignerDetailsPageDesktop)
 
 export default DesignerDetailsPageDesktop
